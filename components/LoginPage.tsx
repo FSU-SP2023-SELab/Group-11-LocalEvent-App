a14bec8fbbc1b6040d41d7a1bb2da38b6b5dfbc7
huertw <wh19@my.fsu.edu>
2023-03-22 14:18:36 -0400

    Login page is now functional with firebase authentication. Changed username to email bc that is what firebase uses for verification

--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -2,31 +2,41 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import styles from "../Utils/Styles/LoginPageStyle"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const LoginPage = ({bringStateUp}) => {
-    // let h : number[] = []
-    // let htmlRep : JSX.Element[]              VERY USEFUL
-    // const [username, setUsername] = useState('')
-    // const [password, setPassword] = useState('')
-    function isUser(){
-        bringStateUp(username,password)
-    }
-    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [loginStatus, setLoginStatus] = useState('')
+
+    const auth = getAuth();
+
+    const handleLogin = () => {
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            setLoginStatus('Login Successful')
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            setLoginStatus('Login Failed')
+        });
+    }
+
     return(
         <View style={styles.container}>
             
             <View style={styles.inputContainer}>
                 <Text>Username</Text>
-                <TextInput style={styles.input} onChangeText={(newText) => setUsername(newText)} value={username}/>
+                <TextInput style={styles.input} onChangeText={(newText) => setEmail(newText)} value={email}/>
                 <Text>Password</Text>
                 <TextInput style={styles.input} onChangeText={(newText) =>setPassword(newText)} defaultValue={password}/>
             </View>
   
-            <Button onPress={() => isUser()} title='login' ></Button>
-            {/* <Button onPress={() => navigation.navigate('Home')} title='Go To Home'></Button> */}
+            <Button onPress={handleLogin} title={loginStatus || 'login'} />
         </View>
-       
     )
 }
-export default LoginPage
+export default LoginPage;
