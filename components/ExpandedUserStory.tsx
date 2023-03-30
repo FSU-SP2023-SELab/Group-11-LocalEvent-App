f5fffb4fac0275019e63ef78647e712d5f157726
huertw <wh19@my.fsu.edu>
2023-03-30 17:17:46 -0400

    likes are now stored in the database. But need to add a check if a user has like a specific post so they cannot like the same post multiple times

--- components/ExpandedUserStory.tsx
+++ components/ExpandedUserStory.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import {View,Text, StyleSheet, Image} from 'react-native'
+import { getDatabase, ref, runTransaction } from 'firebase/database';
+import React, { useState } from 'react'
+import {View,Text, StyleSheet, Image, TouchableOpacity} from 'react-native'
 function ExpandedUserStory({route}) {
-const {nameOfUser, timeOfEvent, timePostWasMade, titleOfEvent, eventDescription} = route.params
+  //added id and numOfLikes to the parameter list and passed them from HomePage.tsx in the onPress()
+  const {nameOfUser, timeOfEvent, timePostWasMade, titleOfEvent, eventDescription, numOfLikes, id} = route.params
+  const [liked, setLiked] = useState(false);
+  const database = getDatabase();
+
 
   return (
     <View style={styles.expandedUserStoryPageContainer}>
@@ -11,6 +16,30 @@ const {nameOfUser, timeOfEvent, timePostWasMade, titleOfEvent, eventDescription}
         <View style={styles.picStyle}>
           <Image source={require('../Utils/Imgs/Party.webp')} style={styles.picStyle}/>
         </View>
+        
+        <TouchableOpacity
+          style={[styles.likeButton, { backgroundColor: liked ? 'red' : 'white' }]}
+          onPress={() => {
+            setLiked(!liked);
+            const db = getDatabase();
+            const storyRef = ref(db, `UserStories/${id}/numOfLikes`);
+
+            //a story with no likes may have value -1 instead of zero
+            //the code needs a way of knowing if a logged in user has liked a specific story or not  
+            runTransaction(storyRef, (currentValue) => {
+              return liked ? currentValue - 1 : currentValue + 1;
+            }).then(() => {
+              console.log('Transaction completed successfully!');
+            }).catch((error) => {
+              console.error('Transaction failed: ', error);
+            });
+          }}
+        >
+          <Text style={[styles.likeButtonText, { color: liked ? 'white' : 'black' }]}>
+            {liked ? 'Unlike' : 'Like'}
+          </Text>
+        </TouchableOpacity>
+
         <View style={styles.titleContainer}>
           <Text style={{fontSize: 40}}>{titleOfEvent}</Text>
           <Text>{timeOfEvent}</Text>
@@ -45,6 +74,16 @@ const styles= StyleSheet.create({
     display: 'flex',
     width: "100%",
     height: '10%',
+  },
+  likeButton: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+  },
+  likeButtonText: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 
 
